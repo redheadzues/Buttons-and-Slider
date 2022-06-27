92d13a9efbc1a61dc1eff0d58fefee0e3cad89ff
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-06-27 19:47:16 +0300

    Initial commit

---
+++ Assets/Scripts/ArrowFly.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowFly : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    void Start()
+    {
+        
+    }
+}
