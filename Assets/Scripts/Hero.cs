3eb684ff5d3f760ed475b648b1158061634d7a8e
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-06-29 10:41:56 +0300

    /

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -3,47 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(HeroAnimator))]
 public class Hero : MonoBehaviour
 {
-    [SerializeField] private BarRenderer _barRenderer;
-
-    private Animator _animator;
-    private static int _takeHit = Animator.StringToHash("TakeHit");
-    private static int _takeHeal = Animator.StringToHash("TakeHeal");
-    private static int _die = Animator.StringToHash("Die");
     private int _maxHealth = 50;
     private int _health;
+    private int _theNumberAtWhichHealthEnd = 0;
+    private HeroAnimator _animation;
 
     private void Start()
     {
-        _animator = GetComponent<Animator>();
+        _animation = GetComponent<HeroAnimator>();
         _health = _maxHealth;
     }
 
     private void ReactionOnAction(int value)
     {
-        if(_health + value <= _maxHealth && _health > 0)
+        if(_health + value <= _maxHealth && _health > _theNumberAtWhichHealthEnd)
         {
             _health += value;
-            PlayAnimation(value);
-            _barRenderer.ChangeBarValue(value);
+            _animation.Play(value, _health);
         }
-    }
-
-
-    private void PlayAnimation(int value)
-    {
-        if(_health <= 0)
-        {
-            _animator.SetTrigger(_die);
-        }
-        else
-        {
-            if (value > 0)
-                _animator.SetTrigger(_takeHeal);
-            else
-                _animator.SetTrigger(_takeHit);
-        }
-    }
+    }   
 }
---
+++ Assets/Scripts/HeroAnimator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class HeroAnimator : MonoBehaviour
+{
+    private Animator _animator;
+    private static int _takeHit = Animator.StringToHash("TakeHit");
+    private static int _takeHeal = Animator.StringToHash("TakeHeal");
+    private static int _die = Animator.StringToHash("Die");
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    public void Play(int value, int health)
+    {
+        if (health + value <= 0)
+        {
+            _animator.SetTrigger(_die);
+        }
+        else
+        {
+            if (value > 0)
+                _animator.SetTrigger(_takeHeal);
+            else
+                _animator.SetTrigger(_takeHit);
+        }
+    }
+}
